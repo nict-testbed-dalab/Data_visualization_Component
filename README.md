27d0049340f3114c4662898b30eb8468bbf1ebb3
yamashita <yamashita@sec.co.jp>
2022-12-13 10:11:51 +0900

    add library

--- README.md
+++ README.md
@@ -12,3 +12,194 @@ WebGISアプリ上で、主に時系列データを可視化するための機�
 - 面群表示機能（PlaneGroup）
 - 点群表示機能（PointCloud）
 - レイヤー間相対位置判定機能（RelativePosition）
+
+## 画像キャプチャ機能
+### 導入方法
+- dalab_displaycapture.jsをダウンロードする。
+- jQueryとダウンロードしたライブラリをHTMLファイルへ組み込む。
+
+### API
+
+#### singleCapture
+  画像キャプチャを行う
+
+  * 構文 <br>
+  singleCapture(fileName)
+  * 引数
+    + fileName キャプチャ画像の保存名
+  * 戻り値 <br>
+  なし（ダウンロードフォルダにキャプチャ画像が格納される。）
+
+
+#### multiCapture
+  時系列データの画面表示を対象に、開始時刻から終了時刻まで一定時間を進めながら画像キャプチャを行う。
+
+  * 構文 <br>
+  multiCapture(currentTime, startTime, endTime, stepTime, pIsContinueCallback, pPreCaptureCallback, pNameFormatCallback )
+  * 引数
+    + currentTime  表示中の時系列データの時刻
+    + startTime   画像キャプチャの開始時刻
+    + endTime   画像キャプチャの終了時刻
+    + stepTime   画像キャプチャ実施ごとに進める時間ステップ
+    + pIsContinueCallback   画像キャプチャを継続するかをチェックするためのコールバック関数
+    + pPreCaptureCallback   画像キャプチャの前処理のためのコールバック関数
+    + pNameFormatCallback   キャプチャ画像の保存名を返却するコールバック関数
+  * 戻り値 <br>
+  なし（ダウンロードフォルダにキャプチャ画像が格納される。）
+
+
+## Mapbox向けデータ可視化用ライブラリ
+- dalab_visualization_lib.jsをダウンロードする。
+- jQuery、Turf.js、D3.js、deck.gl、ダウンロードしたライブラリをHTMLファイルへ組み込む。
+
+### API
+
+#### addBargraphLayer
+  3次元グラフのレイヤーを追加する。
+
+  * 構文 <br>
+  addBargraphLayer(map, filename, dataname, layerId, sourceId, color, radiusSize, elevationScale)
+  * 引数
+    + map Mapboxのmapオブジェクト
+    + filename 3次元グラフの元データとなるファイル。読み取り可能なファイルはCSVファイル、GeoJsonファイルのいずれか。
+    + dataname 可視化対象のデータ。CSVファイルの場合はカラム名、GeoJsonファイルの場合はfeatures.properties配下のキーを指定する。
+    + layerId 追加するレイヤーごとに指定するID
+    + sourceId 追加するレイヤーのsourceID
+    + color 3次元グラフの色
+    + radiusSize 3次元グラフの幅
+    + elevationScale 3次元グラフの高さ（倍率）
+  * 戻り値 <br>
+  なし
+
+
+#### updateBargraphLayer
+  追加した3次元グラフのレイヤーを更新する。
+
+  * 構文 <br>
+  updateBargraphLayer(map, filename, dataname, layerId, sourceId, color, radiusSize, elevationScale)
+  * 引数
+    + map Mapboxのmapオブジェクト
+    + filename 3次元グラフの元データとなるファイル。読み取り可能なファイルはCSVファイル(lng、latカラムは必須)、GeoJsonファイルのいずれか。
+    + dataname 可視化対象のデータ。CSVファイルの場合はカラム名、GeoJsonファイルの場合はfeatures.properties配下のキーを指定する。
+    + layerId 追加したレイヤーのレイヤーID
+    + sourceId 追加したレイヤーに含まれるsourceID
+    + color 3次元グラフの色
+    + radiusSize 3次元グラフの幅（メートル）
+    + elevationScale 3次元グラフの高さ（倍率）
+  * 戻り値 <br>
+  なし
+
+
+#### addMovingObjectLayer
+  方向のある3次元オブジェクトのレイヤーを追加する。
+
+  * 構文 <br>
+  addMovingObjectLayer(map, filename, layerId, sourceId, color, size)
+  * 引数
+    + map Mapboxのmapオブジェクト
+    + filename 3次元オブジェクトの元データとなるファイル。読み取り可能なファイルはCSVファイル(lng、lat、directionカラムは必須)、GeoJsonファイル(features.properties.directionは必須)のいずれか。
+    + layerId 追加するレイヤーごとに指定するID
+    + sourceId 追加するレイヤーのsourceID
+    + color 3次元グラフの色
+    + size 3次元グラフの幅（メートル）
+  * 戻り値 <br>
+  なし
+
+
+#### updateMovingObjectLayer
+  追加した3次元オブジェクトのレイヤーを更新する。
+
+  * 構文 <br>
+  updateMovingObjectLayer(map, filename, layerId, sourceId, color, size)
+  * 引数
+    + map Mapboxのmapオブジェクト
+    + filename 3次元オブジェクトの元データとなるファイル。読み取り可能なファイルはCSVファイル(lng、lat、directionカラムは必須)、GeoJsonファイル(features.properties.directionは必須)のいずれか。
+    + layerId 追加したレイヤーのレイヤーID
+    + sourceId 追加したレイヤーに含まれるsourceID
+    + color 3次元オブジェクトの色
+    + size 3次元オブジェクトのサイズ（メートル）
+  * 戻り値 <br>
+  なし
+
+
+#### addPlaneGroupLayer
+  面群レイヤーを追加する。対象データは色で可視化する。
+
+  * 構文 <br>
+  addPlaneGroupLayer(map, filename, dataname, layerId)
+  * 引数
+    + map Mapboxのmapオブジェクト
+    + filename 面群の元データとなるファイル。読み取り可能なファイルはGeoJsonファイル。
+    + dataname 可視化対象のデータ。features.properties配下のキーを指定する。
+    + layerId 追加するレイヤーごとに指定するID
+  * 戻り値 <br>
+  なし
+
+
+#### updatePlaneGroupLayer
+  追加した面群レイヤーを更新する。
+
+  * 構文 <br>
+  updatePlaneGroupLayer(map, filename, dataname, layerId)
+  * 引数
+    + map Mapboxのmapオブジェクト
+    + filename 面群の元データとなるファイル。読み取り可能なファイルはGeoJsonファイル。
+    + dataname 可視化対象のデータ。features.properties配下のキーを指定する。
+    + layerId 追加したレイヤーのレイヤーID
+  * 戻り値 <br>
+  なし
+
+
+#### addPointCloudLayer
+  点群レイヤーを追加する。対象データは色で可視化する。
+
+  * 構文 <br>
+  addPlaneGroupLayer(map, filename, dataname, layerId)
+  * 引数
+    + map Mapboxのmapオブジェクト
+    + filename 点群の元データとなるファイル。読み取り可能なファイルはCSVファイル(lng、latカラムは必須)、GeoJsonファイルのいずれか。
+    + dataname 可視化対象のデータ。CSVファイルの場合はカラム名、GeoJsonファイルの場合はfeatures.properties配下のキーを指定する。
+    + layerId 追加するレイヤーごとに指定するID
+  * 戻り値 <br>
+  なし
+
+
+#### updatePointCloudLayer
+  追加した点群レイヤーを更新する。
+
+  * 構文 <br>
+  addPlaneGroupLayer(map, filename, dataname, layerId)
+  * 引数
+    + map Mapboxのmapオブジェクト
+    + filename 点群の元データとなるファイル。読み取り可能なファイルはCSVファイル(lng、latカラムは必須)、GeoJsonファイルのいずれか。
+    + dataname 可視化対象のデータ。CSVファイルの場合はカラム名、GeoJsonファイルの場合はfeatures.properties配下のキーを指定する。
+    + layerId 追加したレイヤーのレイヤーID
+  * 戻り値 <br>
+  なし
+
+
+#### addRelativePositionPointLayer
+  本ライブラリで追加した面群データとの位置関係を判別する点群レイヤーを追加する。面群内の点は赤、面群外の点は青で描画する。
+
+  * 構文 <br>
+  addPlaneGroupLayer(map, filename, dataname, layerId)
+  * 引数
+    + map Mapboxのmapオブジェクト
+    + filename 点群の元データとなるファイル。読み取り可能なファイルはCSVファイル(lng、latカラムは必須)、GeoJsonファイルのいずれか。
+    + planeLayerId 事前にaddPlaneGroupLayerで追加した面群レイヤーのID
+    + layerId 追加するレイヤーごとに指定するID
+  * 戻り値 <br>
+  なし
+
+#### updateRelativePositionPointLayer
+  面群データとの位置関係を判別する点群レイヤーを更新する。
+
+  * 構文 <br>
+  updateRelativePositionPointLayer(map, filename, dataname, layerId)
+  * 引数
+    + map Mapboxのmapオブジェクト
+    + filename 点群の元データとなるファイル。読み取り可能なファイルはCSVファイル(lng、latカラムは必須)、GeoJsonファイルのいずれか。
+    + planeLayerId 事前にaddPlaneGroupLayerで追加した面群レイヤーのID
+    + layerId 追加したレイヤーのレイヤーID
+  * 戻り値 <br>
+  なし
---
+++ dalab_visualization_lib.js
@@ -0,0 +1,456 @@
+/**/
+/**/
+/**/
+function addBargraphLayer(map, file, dataname, layerId, sourceId, color, radiusSize, elevationScale) {
+  map.addSource(sourceId, {
+    "type": "geojson",
+    "data": {
+      type: 'FeatureCollection',
+      features: []
+    }
+  });
+
+  map.addLayer({
+    'id': layerId,
+    'type': 'fill-extrusion',
+    'source': sourceId,
+    'paint': {
+      'fill-extrusion-color': color,
+      'fill-extrusion-height': ['get', 'height'],
+      'fill-extrusion-base': ['get', 'base'],
+      'fill-extrusion-opacity': 0.75
+    }
+  });
+
+  updateBargraphLayer(map, file, dataname, layerId, sourceId, color, radiusSize, elevationScale);
+}
+
+function updateBargraphLayer(map, file, dataname, layerId, sourceId, color, radiusSize, elevationScale) {
+  if(file.split('.').pop() == "csv"){
+    updateBargraphLayerByCsvFile(map, file, dataname, layerId, sourceId, color, radiusSize, elevationScale);
+  } else if(file.split('.').pop() == "geojson") {
+    updateBargraphLayerByGeoJsonFile(map, file, dataname, layerId, sourceId, color, radiusSize, elevationScale);
+  }  
+}
+
+function updateBargraphLayerByCsvFile(map, csvfile, column, layerId, sourceId, color, radiusSize, elevationScale){
+  d3.csv(csvfile).then(function(csvdata) {
+    let data = createSourceByCsvData(csvdata, column, radiusSize, elevationScale);
+    map.getSource(sourceId).setData(data);
+    map.setLayoutProperty(layerId, 'visibility', 'visible');
+    map.setPaintProperty(layerId, 'fill-extrusion-color', color);
+
+  }).catch(function(error){
+    console.log(error);
+    console.log("error : readCsv " + csvfile);
+    if (map.getLayer(layerId)) {
+      map.setLayoutProperty(layerId, 'visibility', 'none');
+    }
+  });
+}
+
+function createSourceByCsvData(csvdata, column, radiusSize, elevationScale){
+  let data = {
+    "type": "FeatureCollection",
+    "features": []
+  };
+
+  csvdata.map(function(d, i) {
+    const properties = {
+      index : i,
+      height : (parseFloat(d[column]) * elevationScale),
+      base : 0
+    };
+    const options = {
+      steps: 4,
+      units: 'meters',
+      properties: properties
+    };
+    const feature = turf.circle(turf.point([parseFloat(d.lng), parseFloat(d.lat)]), radiusSize, options);
+    data.properties = properties;
+    data.features.push(feature);
+  });
+
+  return data;
+}
+
+function updateBargraphLayerByGeoJsonFile(map, geojsonfile, key, layerId, sourceId, color, radiusSize, elevationScale) {
+  d3.json(geojsonfile).then(function(geojsonData) {
+    let data = createSourceDataByGeoJsonData(geojsonData, key, radiusSize, elevationScale);
+    map.getSource(sourceId).setData(data);
+    map.setLayoutProperty(layerId, 'visibility', 'visible');
+    map.setPaintProperty(layerId, 'fill-extrusion-color', color);
+  }).catch(function(error) {
+    console.log(error);
+    console.log("error : readGeoJson " + geojsonfile);
+    if (map.getLayer(layerId)) {
+      map.setLayoutProperty(layerId, 'visibility', 'none');
+    }
+  });
+}
+
+function createSourceDataByGeoJsonData(geojsonData, key, radiusSize, elevationScale){
+  let data = {
+    "type": "FeatureCollection",
+    "features": []
+  };
+
+  geojsonData.features.forEach(function (object, i) {
+    object.properties.height = object.properties[key] * elevationScale;
+    object.properties.base = 0;
+    object.properties.index = i;
+
+    const point = object.geometry.coordinates
+    const options = {
+      steps: 4,
+      units: 'meters',
+      properties: object.properties
+    };
+    const feature = turf.circle(point, radiusSize, options);
+
+    data.features.push(feature);
+  });
+  return data;
+}
+
+
+/**/
+/**/
+/**/
+function addMovingObjectLayer(map, file, layerId, sourceId, color, size) {
+  map.addSource(sourceId, {
+    "type": "geojson",
+    "data": {
+      type: 'FeatureCollection',
+      features: []
+    }
+  });
+
+  map.addLayer({
+    'id': layerId,
+    'type': 'fill-extrusion',
+    'source': sourceId,
+    'paint': {
+      'fill-extrusion-color': color,
+      'fill-extrusion-height': ['get', 'height'],
+      'fill-extrusion-base': ['get', 'base'],
+      'fill-extrusion-opacity': 0.9
+    }
+  });
+
+  updateMovingObjectLayer(map, file, layerId, sourceId, color, size);
+}
+
+function updateMovingObjectLayer(map, file, layerId, sourceId, color, size) {
+  if(file.split('.').pop() == "csv"){
+    updateMovingObjectLayerByCsvFile(map, file, layerId, sourceId, color, size);
+  } else if(file.split('.').pop() == "geojson") {
+    updateMovingObjectLayerByGeoJsonFile(map, file, layerId, sourceId, color, size);
+  }  
+}
+
+function updateMovingObjectLayerByCsvFile(map, csvfile, layerId, sourceId, color, size){
+  d3.csv(csvfile).then(function(csvdata) {
+    let data = createMovingObjectSourceByCsvData(csvdata, size);
+    map.getSource(sourceId).setData(data);
+    map.setLayoutProperty(layerId, 'visibility', 'visible');
+    map.setPaintProperty(layerId, 'fill-extrusion-color', color);
+
+  }).catch(function(error){
+    console.log(error);
+    console.log("error : readCsv " + csvfile);
+    if (map.getLayer(layerId)) {
+      map.setLayoutProperty(layerId, 'visibility', 'none');
+    }
+  });
+}
+
+function createMovingObjectSourceByCsvData(csvdata, size){
+  let data = {
+    "type": "FeatureCollection",
+    "features": []
+  };
+
+  csvdata.map(function(d, i) {
+    const properties = {
+      index : i,
+      height : size / 2,
+      base : 0
+    };
+    let feature = createMovingObjectPolygon(parseFloat(d.lng), parseFloat(d.lat), size, parseFloat(d.direction), properties);
+    data.properties = properties;
+    data.features.push(feature);
+  });
+
+  console.log(JSON.stringify(data));
+  return data;
+}
+
+function updateMovingObjectLayerByGeoJsonFile(map, geojsonfile, layerId, sourceId, color, size) {
+  d3.json(geojsonfile).then(function(geojsonData) {
+    let data = createMovingObjectSourceDataByGeoJsonData(geojsonData, size);
+    map.getSource(sourceId).setData(data);
+    map.setLayoutProperty(layerId, 'visibility', 'visible');
+    map.setPaintProperty(layerId, 'fill-extrusion-color', color);
+  }).catch(function(error) {
+    console.log(error);
+    console.log("error : readGeoJson " + geojsonfile);
+    if (map.getLayer(layerId)) {
+      map.setLayoutProperty(layerId, 'visibility', 'none');
+    }
+  });
+}
+
+function createMovingObjectSourceDataByGeoJsonData(geojsonData, size){
+  let data = {
+    "type": "FeatureCollection",
+    "features": []
+  };
+
+  geojsonData.features.forEach(function (object, i) {
+    object.properties.height = size / 2;
+    object.properties.base = 0;
+    object.properties.index = i;
+
+    const point = object.geometry.coordinates;
+    let feature = createMovingObjectPolygon(object.geometry.coordinates[0], object.geometry.coordinates[1], size, object.properties.direction, object.properties);
+    data.features.push(feature);
+  });
+
+  return data;
+}
+
+function createMovingObjectPolygon(centerLng, centerLat, size, direction, properties) {
+  const point = turf.point([centerLng, centerLat]);
+  let options = {
+    units: 'meters',
+  };
+
+  let f = turf.destination(point, size,     0, options);
+  let lb = turf.destination(point, size, -160, options);
+  let rb = turf.destination(point, size,  160, options);
+
+  feature = turf.polygon([[
+    [f.geometry.coordinates[0], f.geometry.coordinates[1]],
+    [lb.geometry.coordinates[0], lb.geometry.coordinates[1]],
+    [rb.geometry.coordinates[0], rb.geometry.coordinates[1]],
+    [f.geometry.coordinates[0], f.geometry.coordinates[1]]
+  ]], properties);
+
+	console.log(direction);
+  feature = turf.transformRotate(feature, direction, {pivot: point});
+
+  return feature;
+}
+/**/
+/**/
+/**/
+function addPlaneGroupLayer(map, filename, dataname, layerId){
+  d3.json(filename).then(function(data) {
+    planeGroupLayer = createPlaneGroupLayer(data.features, dataname, layerId);
+    map.addLayer(planeGroupLayer);
+  }).catch(function(error){
+    console.log(error);
+  });
+}
+
+function updatePlaneGroupLayer(map, filename, dataname, layerId){
+  if (map.getLayer(layerId)) {
+    map.removeLayer(layerId);
+  }
+  addPlaneGroupLayer(map, filename, dataname, layerId);
+}
+
+function createPlaneGroupLayer(planeData, key, layerId){
+  const layer = new deck.MapboxLayer({
+    id: layerId,
+    type: deck.PolygonLayer,
+    data: planeData,
+    getPolygon: d => d.geometry.coordinates,
+    getElevation: d => 0,
+    getFillColor: d => convertColor(d.properties[key]),
+    getLineColor: [255, 0, 0],
+    opacity: 0.5,
+  });
+
+  return layer;
+}
+
+/**/
+/**/
+/**/
+function addPointCloudLayer(map, filename, dataname, layerId){
+  if(filename.split('.').pop() == "csv"){
+    addPointCloudLayerByCSV(map, filename, dataname, layerId);
+  } else if(filename.split('.').pop() == "geojson") {
+    addPointCloudLayerByGeoJson(map, filename, dataname, layerId);
+  }
+}
+
+function updatePointCloudLayer(map, filename, dataname, layerId){
+  if (map.getLayer(layerId)) {
+    map.removeLayer(layerId);
+  }
+  addPointCloudLayer(map, filename, column, layerId);
+}
+
+function addPointCloudLayerByGeoJson(map, filename, key, layerId){
+  d3.json(filename).then(function(data) {
+    pointCloudLayer = createPointCloudLayerByGeoJson(data.features, key, layerId);
+    map.addLayer(pointCloudLayer);
+  }).catch(function(error){
+    console.log(error);
+    console.log("error : readCsv " + filename);
+  });
+}
+
+function createPointCloudLayerByGeoJson(pointData, key, layerId){
+  const { MapboxLayer, PointCloudLayer } = deck;
+
+  return new MapboxLayer({
+    id: layerId,
+    type: PointCloudLayer,
+    data: pointData,
+    getPosition: d => [d.geometry.coordinates[0], d.geometry.coordinates[1], 100],
+    getColor: d => convertColor(d.properties[key]),
+    sizeUnits: 'meters',
+    pointSize: 20,
+    opacity: 1
+  });
+}
+
+function addPointCloudLayerByCSV(map, filename, column, layerId){
+  d3.csv(filename).then(function(data) {
+    pointCloudLayer = createPointCloudLayerByCSV(data, column, layerId);
+    map.addLayer(pointCloudLayer);
+  }).catch(function(error){
+    console.log(error);
+    console.log("error : readCsv " + filename);
+  });
+}
+
+function createPointCloudLayerByCSV(pointData, column, layerId){
+  const { MapboxLayer, PointCloudLayer } = deck;
+
+  return new MapboxLayer({
+    id: layerId,
+    type: PointCloudLayer,
+    data: pointData,
+    getPosition: d => [Number(d.lng), Number(d.lat), 100],
+    getColor: d => convertColor(d[column]),
+    sizeUnits: 'meters',
+    pointSize: 20,
+    opacity: 1
+  });
+}
+
+function convertColor(val){
+  let r = (parseInt(Math.min(val, 16 * 16* 16 - 1) / (16 * 16))) * 16;
+  let g = (parseInt(val / 16) % 16) * 16;
+  let b = (val % 16) * 16;
+
+  return [r, g, b];
+}
+
+/**/
+/**/
+/**/
+
+function addRelativePositionPointLayer(map, filename, planeLayerId, layerId){
+  if(filename.split('.').pop() == "csv"){
+    addRelativePositionPointLayerByCSV(map, filename, planeLayerId, layerId);
+  } else if(filename.split('.').pop() == "geojson") {
+    addRelativePositionPointLayerByGeoJson(map, filename, planeLayerId, layerId);
+  }
+}
+
+function updateRelativePositionPointLayer(map, filename, planeLayerId, layerId){
+  if (map.getLayer(layerId)) {
+    map.removeLayer(layerId);
+  }
+  addRelativePositionPointLayer(map, filename, planeLayerId, layerId);
+}
+
+function addRelativePositionPointLayerByGeoJson(map, filename, planeLayerId, layerId){
+  d3.json(filename).then(function(data) {
+    let planeData = map.getLayer(planeLayerId).implementation.props.data;
+    pointCloudLayer = createRelativePositionPointLayerByGeoJson(data.features, planeData, layerId);
+    map.addLayer(pointCloudLayer);
+  }).catch(function(error){
+    console.log(error);
+    console.log("error : readCsv " + filename);
+  });
+}
+
+function createRelativePositionPointLayerByGeoJson(pointData, planeData, layerId){
+  const { MapboxLayer, PointCloudLayer } = deck;
+
+  return new MapboxLayer({
+    id: layerId,
+    type: PointCloudLayer,
+    data: pointData,
+    getPosition: d => [d.geometry.coordinates[0], d.geometry.coordinates[1], 100],
+    getColor: d => convertPointColorInPlane(d.geometry.coordinates, planeData),
+    sizeUnits: 'meters',
+    pointSize: 20,
+    opacity: 1
+  });
+}
+
+function addRelativePositionPointLayerByCSV(map, filename, planeLayerId, layerId){
+  d3.csv(filename).then(function(data) {
+    let planeData = map.getLayer(planeLayerId).implementation.props.data;
+    pointCloudLayer = createRelativePositionPointLayerByCSV(data, planeData, layerId);
+    map.addLayer(pointCloudLayer);
+  }).catch(function(error){
+    console.log(error);
+    console.log("error : readCsv " + filename);
+  });
+}
+
+function createRelativePositionPointLayerByCSV(pointData, planeData, layerId){
+  const { MapboxLayer, PointCloudLayer } = deck;
+
+  return new MapboxLayer({
+    id: layerId,
+    type: PointCloudLayer,
+    data: pointData,
+    getPosition: d => [Number(d.lng), Number(d.lat), 100],
+    getColor: d => convertPointColorInPlane(d.geometry.coordinates, planeData),
+    sizeUnits: 'meters',
+    pointSize: 20,
+    opacity: 1
+  });
+}
+
+function convertPointColorInPlane(pointData, planeDataArray){
+  let defaultColor = [0, 0, 255];
+  let insideColor = [255, 0, 0];
+
+  if(!planeDataArray){
+    return defaultColor;
+  }
+
+  var pt = {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": pointData
+    }
+  };
+
+  for(let planeData of planeDataArray){
+    var poly = {
+      "type": "Feature",
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": planeData.geometry.coordinates
+      }
+    };
+    if(turf.inside(pt, poly)){
+      return insideColor;
+    }
+  }
+  return defaultColor;
+}
+
